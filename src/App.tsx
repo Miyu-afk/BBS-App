0c7df92c57393221a558d544a185b6dd948fe8e5
Miyu Shimokawa <kooooohalulu@gmail.com>
2025-10-20 06:56:40 +0900

    BBS-Appのバックエンド連携の部分を変更しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import BBSBody from "./components/BBSBody";
+import { supabase } from "./lib/supabaseClient";
+import { useNavigate } from "react-router-dom";
+
+
+
+interface BBSProps {
+  id: number;
+  name: string;
+  date: Date;
+  text: string;
+}
+
+interface COMPANYProps{
+  id: number;
+  name: string;
+  company_id: number;
+}
 
 function App() {
+  const [alreadyBbs, setAlreadyBbs] = useState<BBSProps[]>()
+  const [companyData, setCompanyData] = useState<COMPANYProps>()
+  const [companyIdData, setCompanyIdData] = useState<string | null>()
+
+  const navigate = useNavigate()
+
+  const fetchCompany = async () => {
+    const companyId = localStorage.getItem("company_id")
+    setCompanyIdData(companyId)
+
+    if(!companyId && location.pathname !== "/"){
+      navigate("/");
+    return;
+  }
+
+  const { data, error } = await supabase
+  .from("company-data")
+  .select("*")
+  .eq("company_id", companyId)
+  .single();
+  if(error){
+    console.error("Supabase Fetch Error:", error);
+    alert("データ取得に失敗しました。");
+    return;
+  } 
+  setCompanyData(data as COMPANYProps);
+};
+
+useEffect(() => {
+  fetchCompany();
+}, []);
+
+  const fetchBbs = async () => {
+    const { data, error } = await supabase
+    .from("App-Content")
+    .select("*")
+    .eq("affiliation_id", companyIdData)
+    if(error){
+      console.error("Supabase Fetch Error:", error);
+      alert("データ取得に失敗しました。");
+      return;
+    }
+    setAlreadyBbs(data as BBSProps[]);
+  }
+
+  useEffect(()=>{
+    if(companyIdData){
+      fetchBbs();
+    }
+  }, []);
+
+  const addBbs = async (newBbsData:BBSProps) => {
+    const {error} = await supabase
+    .from("App-Content")
+    .insert([newBbsData]);
+    if(error){
+      console.error("Supabase Insert Error", error);
+      alert("登録に失敗しました。");
+    }else{
+      await fetchBbs();
+    }
+  };
 
 
   return (
--- src/components/BBSBody.tsx
+++ src/components/BBSBody.tsx
@@ -4,14 +4,16 @@ import BBSUploader from "./BBSUplorder";
 const BBSBody = () => {
   return (
     <>
+    <div className="h-dvh">
       <div className="flex justify-center mt-5">
       <p>社内BBS</p>
       </div>
       <div className="pb-24">
       <BBSContent />
       </div>
-      <div className="fixed bottom-0 flex justify-center w-full">
+      <div className="fixed bottom-30 flex justify-center w-full">
       <BBSUploader />
+      </div>
       </div>
     </>
   );
